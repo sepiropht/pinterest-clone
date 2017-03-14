bf58ccde4faa9d59fefafe0cf54dcc728f141338
William Elimbi <williamelimbi@gmail.com>
2017-03-14 09:42:43 +0100

    create actions and reducers for images state

---
+++ src/reducers/Images.js
@@ -0,0 +1,20 @@
+import { ADD_IMAGE, LOAD_IMAGES, REMOVE_IMAGE } from "../actions/images";
+function Images(state = [], action) {
+  switch (action.type) {
+    case LOAD_IMAGES:
+      [1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => ({
+        link: `http://lorempicsum.com/futurama/255/200/${i}`,
+        like: i,
+        owner: `me`,
+        id: `images${i}`
+      }));
+    case ADD_IMAGE:
+      return [...state, action.payload];
+    case REMOVE_IMAGE:
+      return state.filter(img => img !== action.payload.id);
+    default:
+      return state;
+  }
+}
+
+export default Images;

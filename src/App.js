5278661363f471f8699c170bbdd42c0b090b54d6
William Elimbi <williamelimbi@gmail.com>
2017-03-20 00:21:56 +0100

    refactor client

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import { createStore } from "redux";
-import Images from "./reducers/Images";
-import devsToolsEnhancer from "remote-redux-devtools";
-import { Provider } from "react-redux";
+
 import { loadImages } from "./actions/Images";
 import Profil from "./components/Profil";
 import Modal from "./components/Modal/modal";
 
-let store = createStore(Images, devsToolsEnhancer());
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <div className="App">
-        <div className="App-header">
-          <h2><Link to="/">Welcome to Pinterest-clone</Link></h2>
-          <Modal />
-        </div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/profil/:id" component={Profil} />
+  <Router>
+    <div className="App">
+      <div className="App-header">
+        <h2><Link to="/">Welcome to Pinterest-clone</Link></h2>
+        <Modal />
       </div>
-    </Router>
-  </Provider>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/profil/:id" component={Profil} />
+    </div>
+  </Router>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import devsToolsEnhancer from "remote-redux-devtools";
+import "./index.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Images from "./reducers/Images";
+let store = createStore(Images, devsToolsEnhancer());
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
